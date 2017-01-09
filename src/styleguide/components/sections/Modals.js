313e9a78f683002598e4b75c540c70fa3c8895d5
Abe Massry <amassry@linode.com>
2017-01-09 16:17:53 -0500

    going to pull rebase

--- src/styleguide/components/sections/Modals.js
+++ src/styleguide/components/sections/Modals.js
@@ -1,25 +1,31 @@
-//import React, { PropTypes } from 'react';
-//
-//import Section from '~/styleguide/components/Section';
-//import ModalBody from '~/layouts/ModalBody';
-//
-//export default function Modals(props) {
-//  return (
-//    <Section name="modals" title="Modals">
-//      <ConfirmModal
-//        buttonText="Delete config"
-//        onOk={() => {
-//          dispatch(hideModal());
-//        }}
-//        onCancel={() => dispatch(hideModal())}
-//      >
-//        Are you sure you want to delete this config?
-//        This operation cannot be undone.
-//      </ConfirmModal>
-//    </Section>
-//  );
-//}
-//
-//Modals.propTypes = {
-//
-//};
+import React, { PropTypes } from 'react';
+
+import Section from '~/styleguide/components/Section';
+import ModalBody from '~/layouts/ModalBody';
+import ConfirmModal from '~/components/ConfirmModal';
+import { showModal, hideModal } from '~/actions/modal';
+
+export default function Modals(props) {
+  return (
+    <Section name="modals" title="Modals">
+      <a
+        onClick={e => {
+          e.preventDefault();
+          dispatch(showModal('Confirm Modal',
+            <ConfirmModal
+              buttonText="Delete config"
+              onOk={() => dispatch(hideModal())}
+              onCancel={() => dispatch(hideModal())}
+            >
+              This is a confirm modal.
+            </ConfirmModal>
+          ));
+        }}
+      >Show confirm modal</a>
+    </Section>
+  );
+}
+
+Modals.propTypes = {
+
+};
