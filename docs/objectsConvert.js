8817d479c1dfd50650319ebac8069f9e47c37dd0
Matthew Parke <mparke78@gmail.com>
2017-05-04 13:35:35 -0400

    Updated objects

--- docs/objectsConvert.js
+++ docs/objectsConvert.js
@@ -6,8 +6,18 @@ const _ = require('lodash');
 
 const BASE_PATH = './src/data';
 
-const data = JSON.stringify(allEndpoints, null, 2);
-const endpointModule = `module.exports = { endpoints: ${data} };`;
-fs.writeFileSync(path.join(endpointsPath, 'api.js'), endpointModule);
+const dirPath = path.join(BASE_PATH, 'objects');
+const files = fs.readdirSync(dirPath);
+files.filter(function(fileName) {
+  return path.extname(fileName) === '.json';
+}).forEach(function(fileName) {
+  const filePath = path.join(dirPath, fileName);
+  console.log(filePath);
+  const data = JSON.stringify(JSON.parse(fs.readFileSync(filePath, 'utf-8')));
+  const asModule = `module.exports = ${data};`;
 
+  const newFileName = `${fileName.split('.')[0]}.js`;
+  const newFilePath = path.join(dirPath, newFileName);
 
+  fs.writeFileSync(newFilePath, asModule);
+});
