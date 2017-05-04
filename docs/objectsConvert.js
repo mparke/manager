93a15088205b340d6dd1a5f816ab472fc2322c9f
Matthew Parke <mparke78@gmail.com>
2017-05-04 13:30:10 -0400

    WIP

--- docs/objectsConvert.js
+++ docs/objectsConvert.js
@@ -9,3 +9,5 @@ const BASE_PATH = './src/data';
 const data = JSON.stringify(allEndpoints, null, 2);
 const endpointModule = `module.exports = { endpoints: ${data} };`;
 fs.writeFileSync(path.join(endpointsPath, 'api.js'), endpointModule);
+
+
