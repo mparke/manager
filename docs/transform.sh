8498011ec0c8eb9a5c83c508e51b1e1c1bf22dba
Abe Massry <amassry@linode.com>
2017-05-04 10:07:17 -0400

    adding transform script to grab latest yaml files

---
+++ docs/transform.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+readonly TEMPDIR=$RANDOM
+
+convertToJSON() {
+  local folder=$1
+  local filename=""
+  cd src/data/$folder
+  ls /tmp/dev_docs_$TEMPDIR/_data/$folder \
+    | while read i; do
+      filename=$(echo $i | sed 's/\.yaml$//')
+      any-json -format=yaml /tmp/dev_docs_$TEMPDIR/_data/$folder/$i > "$filename.json"
+      done
+  #rename 's/\.yaml$/.json/' *.yaml
+  cd ../../..
+}
+
+main() {
+  git clone https://github.com/linode/developers.git /tmp/dev_docs_$TEMPDIR
+  convertToJSON endpoints
+  convertToJSON objects
+  convertToJSON python
+  rm -rf /tmp/dev_docs_$TEMPDIR
+}
+main
