c7df66129d65dfa93994a4a7901888a03b51651d
Matthew Parke <mparke78@gmail.com>
2017-07-10 10:49:51 -0400

    WIP

--- styleguide/src/sections/WritingStyle.js
+++ styleguide/src/sections/WritingStyle.js
@@ -5,7 +5,17 @@ import { StyleguideSection } from '../components';
 export default function WritingStyle() {
   return (
     <StyleguideSection name="writing-style" title="Writing Style">
-      <p></p>
+      <div className="col-sm-12">
+        <div className="StyleguideSubSection row">
+          <div className="col-sm-12">
+            <div className="StyleguideSubSection-header">
+              <h3></h3>
+            </div>
+            <div className="ExampleForm">
+            </div>
+          </div>
+        </div>
+      </div>
     </StyleguideSection>
   );
 }
